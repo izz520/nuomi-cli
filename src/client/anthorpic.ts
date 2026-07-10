bba182dfa4e35b8eade000edc55552b0aadaf704
izz520 <1547471276@qq.com>
2026-07-10 19:04:00 +0800

    feat: update anthropic stream event

--- src/client/anthorpic.ts
+++ src/client/anthorpic.ts
@@ -2,6 +2,7 @@ import { Anthropic } from "@anthropic-ai/sdk"
 import { StreamEvent } from "../types/llm.js";
 import { ProviderConfig } from "../types/provider.js";
 import { MessageManger } from "../messageManger/message.js";
+import writeLog from "../utils/writeLog.js";
 
 class AnthropicClient {
     private client: Anthropic;
@@ -47,9 +48,99 @@ class AnthropicClient {
         }
         //发送消息
         const result = this.client.messages.stream(params)
+        //思考
+        let isThinking = false;
+        let thinkingStr = ""
+        let thinkingSig = ""
+        //回答
+        let isAnswer = false;
+        let answer = ""
+        //工具调用
+        let isUseTools = false;
+        let tool = {
+            toolId: "",
+            toolName: "",
+            toolJson: ""
+        }
         //消费流失输出
         for await (const messageStreamEvent of result) {
-            console.log(messageStreamEvent.type);
+            // console.log(messageStreamEvent.type);
+            writeLog(messageStreamEvent)
+            switch (messageStreamEvent.type) {
+                case "message_start": {
+                    console.log(`消息开始,初始输入Token:${messageStreamEvent.message.usage.input_tokens}，输出Token:${messageStreamEvent.message.usage.output_tokens}`);
+                    break;
+                }
+                case "content_block_start": {
+                    const block = messageStreamEvent.content_block;
+                    if (block.type === "thinking") {
+                        isThinking = true
+                        thinkingStr = ""
+                        thinkingSig = ""
+                        console.log("接下来输出的是思考的内容");
+                    }
+                    if (block.type === "text") {
+                        isAnswer = true
+                        answer = ""
+                        console.log("接下来是回答的内容");
+                    }
+                    if (block.type === "tool_use") {
+                        isUseTools = true
+                        tool.toolId = block.id
+                        tool.toolName = block.name
+                        tool.toolJson = ""
+                        console.log("接下来是工具调用");
+                    }
+                    break;
+
+                }
+                case "content_block_delta": {
+                    const delta = messageStreamEvent.delta;
+                    if (delta.type === "thinking_delta") {
+                        thinkingStr += delta.thinking
+                        // console.log("思考：", thinkingStr);
+                    }
+                    if (delta.type === "signature_delta") {
+                        console.log("思考文案的签名", delta.signature);
+                        thinkingSig = delta.signature
+                    }
+                    if (delta.type === "text_delta") {
+                        answer += delta.text
+                        // console.log("回答：", answer);
+                    }
+                    if (delta.type === "input_json_delta") {
+                        tool.toolJson += delta.partial_json
+                        // console.log("工具调用：", tool.toolJson);
+                    }
+                    break;
+                }
+                case "content_block_stop": {
+                    if (isThinking) {
+                        isThinking = false
+                        console.log("思考内容:", thinkingStr);
+
+                    }
+                    if (isAnswer) {
+                        isAnswer = false
+                        console.log("回答:", answer);
+                    }
+                    if (isUseTools) {
+                        isUseTools = false
+                        console.log(`工具调用:${tool.toolName}-${tool.toolJson}`);
+
+                    }
+                    break;
+                }
+                case "message_delta": {
+                    const usage = messageStreamEvent.usage
+                    console.log(`本次对话结束，本轮消耗的输入Token:${usage.input_tokens},输出Token:${usage.output_tokens}`);
+                    break;
+                }
+                case "message_stop": {
+                    console.log("对话结束");
+                    break;
+                }
+            }
         }
     }
 }
